624662cf5f14adc6cce10412380ffe47a743f9af
Muhammad Zahiruddin <90888537+zahir248@users.noreply.github.com>
2023-11-05 11:47:50 +0800

    Add files via upload

---
+++ database/jompick.sql
@@ -0,0 +1,330 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2023 at 04:38 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jompick`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirmation`
+--
+
+CREATE TABLE `confirmation` (
+  `confirmation_id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `pickUpDuration` varchar(50) NOT NULL,
+  `currentLocation` varchar(255) NOT NULL,
+  `confirmationDate` date NOT NULL,
+  `pickUpLocation_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `due_date_status`
+--
+
+CREATE TABLE `due_date_status` (
+  `dueDateStatus_id` int(11) NOT NULL,
+  `paymentStatus_id` int(11) NOT NULL,
+  `dueDate` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `itemType_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `image` blob NOT NULL,
+  `trackingNumber` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_management`
+--
+
+CREATE TABLE `item_management` (
+  `itemManagement_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `dueDateStatus_id` int(11) DEFAULT NULL,
+  `confirmation_id` int(11) DEFAULT NULL,
+  `report_id` int(11) DEFAULT NULL,
+  `registerDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_type`
+--
+
+CREATE TABLE `item_type` (
+  `itemType_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_status`
+--
+
+CREATE TABLE `payment_status` (
+  `paymentStatus_id` int(11) NOT NULL,
+  `paymentAmount` decimal(10,2) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pickup_location`
+--
+
+CREATE TABLE `pickup_location` (
+  `pickupLocation_id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `rolename` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `rolename`) VALUES
+(1, 'admin'),
+(2, 'staff'),
+(3, 'client');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `icNumber` varchar(50) NOT NULL,
+  `emailAddress` varchar(255) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `userName`, `password`, `phoneNumber`, `icNumber`, `emailAddress`, `fullName`, `role_id`) VALUES
+(62, 'zahir248', 'hawau248', 149947806, '010824040443', 'zahir248@gmail.com', 'muhammad zahiruddin bin mustaza', 3),
+(63, 'nabil', 'nabil', 2147483647, '91274812', 'nabil@gmail.com', 'nabil', 3),
+(65, 'gojo', 'gojo', 149947806, '010824040443', 'gojo@gmail.com', 'gojo satoru', 3),
+(72, 'sanusi', 'abc123', 149947806, '010824040443', 'admin@gmail.com', 'administrator', 1),
+(74, 'akmal', '123', 149947806, '010824040443', 'akmal@gmail.com', 'akmal adib', 2),
+(79, 'zaher', 'abc123', 163562538, '013884360643', 'zahir100@gmail.com', 'ahmad zaher ', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `confirmation`
+--
+ALTER TABLE `confirmation`
+  ADD PRIMARY KEY (`confirmation_id`),
+  ADD KEY `confirmation_fk_pickUpLocation` (`pickUpLocation_id`);
+
+--
+-- Indexes for table `due_date_status`
+--
+ALTER TABLE `due_date_status`
+  ADD PRIMARY KEY (`dueDateStatus_id`),
+  ADD KEY `dueDateStatus_fk_paymentStatus` (`paymentStatus_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `item_fk_itemtype` (`itemType_id`);
+
+--
+-- Indexes for table `item_management`
+--
+ALTER TABLE `item_management`
+  ADD PRIMARY KEY (`itemManagement_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`item_id`,`dueDateStatus_id`,`confirmation_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `dueDateStatus_id` (`dueDateStatus_id`),
+  ADD KEY `confirmation_id` (`confirmation_id`);
+
+--
+-- Indexes for table `item_type`
+--
+ALTER TABLE `item_type`
+  ADD PRIMARY KEY (`itemType_id`);
+
+--
+-- Indexes for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  ADD PRIMARY KEY (`paymentStatus_id`);
+
+--
+-- Indexes for table `pickup_location`
+--
+ALTER TABLE `pickup_location`
+  ADD PRIMARY KEY (`pickupLocation_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_fk_role` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `confirmation`
+--
+ALTER TABLE `confirmation`
+  MODIFY `confirmation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `due_date_status`
+--
+ALTER TABLE `due_date_status`
+  MODIFY `dueDateStatus_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item_management`
+--
+ALTER TABLE `item_management`
+  MODIFY `itemManagement_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item_type`
+--
+ALTER TABLE `item_type`
+  MODIFY `itemType_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  MODIFY `paymentStatus_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pickup_location`
+--
+ALTER TABLE `pickup_location`
+  MODIFY `pickupLocation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `confirmation`
+--
+ALTER TABLE `confirmation`
+  ADD CONSTRAINT `confirmation_fk_pickUpLocation` FOREIGN KEY (`pickUpLocation_id`) REFERENCES `pickup_location` (`pickupLocation_id`);
+
+--
+-- Constraints for table `due_date_status`
+--
+ALTER TABLE `due_date_status`
+  ADD CONSTRAINT `dueDateStatus_fk_paymentStatus` FOREIGN KEY (`paymentStatus_id`) REFERENCES `payment_status` (`paymentStatus_id`);
+
+--
+-- Constraints for table `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_fk_itemtype` FOREIGN KEY (`itemType_id`) REFERENCES `item_type` (`itemType_id`);
+
+--
+-- Constraints for table `item_management`
+--
+ALTER TABLE `item_management`
+  ADD CONSTRAINT `item_management_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `item_management_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `item_management_ibfk_3` FOREIGN KEY (`dueDateStatus_id`) REFERENCES `due_date_status` (`dueDateStatus_id`),
+  ADD CONSTRAINT `item_management_ibfk_4` FOREIGN KEY (`confirmation_id`) REFERENCES `confirmation` (`confirmation_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_fk_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
