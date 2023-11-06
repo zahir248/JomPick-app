a36d05bc773d562dca287a92bbd0e936cd52e360
nabil0p <95075430+nabil0p@users.noreply.github.com>
2023-11-06 23:34:18 +0800

    Update dashboard.dart

--- code nabil/dashboard.dart
+++ code nabil/dashboard.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'penalty.dart';
+import 'profile.dart';
+import 'setting.dart';
 import '../models/user.dart';
 
 void main() => runApp(MaterialApp(home: DashBoard()));
@@ -12,13 +16,29 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-  int _selectedIndex = 0;
+  int _selectedPage = 0;
   List<User> users = [];
+  String? username = '';
+
 
   @override
   void initState() {
     super.initState();
     fetchUserData();
+    loadUsername();
+  }
+
+  Future<void> loadUsername() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      username = prefs.getString('username');
+    });
+  }
+  void handleSetting() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // Navigate to the login page (Assuming your login page is named Setting)
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Setting()));
   }
 
   Future<void> fetchUserData() async {
@@ -44,39 +64,82 @@ class _DashBoardState extends State<DashBoard> {
       ),
     );
   }
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+
+  void _onItemTapped(int page) {
+    if (page == 3) {
+      // If the "Profile" button is tapped (index 3), navigate to the profile page
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Profile(), // Replace "ProfilePage()" with the actual profile page widget
+        ),
+      );
+    } else if(page == 2) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              Penalty(), // Replace "Penalty()" with the actual profile page widget
+        ),
+      );
+    } else if(page == 1) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              Setting(), // Replace "Setting()" with the actual profile page widget
+        ),
+      );
+    } else{
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              DashBoard(), // Replace "DashBoardPage()" with the actual profile page widget
+        ),
+      );
+      setState(() {
+        _selectedPage = page;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        appBar: AppBar(
-          title: Text('JomPick', style:TextStyle(fontSize: 24)),
-          backgroundColor: Colors.blue,
-        ),
+      appBar: AppBar(
+        title: const Text('JomPick'),
+        automaticallyImplyLeading: false, // Remove the back button
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              handleSetting(); // Logout when the button is pressed
+            },
+          ),
+        ],
+      ),
 
         body: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.all(8.0),
-                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(100),
-                  ),
+                Text(
+                  'Welcome $username!',
+                  style: TextStyle(fontSize: 24),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: TextField(
-                      style: TextStyle(fontSize: 18),
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                        border: InputBorder.none,
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.white10, // Set the background color to grey
+                      border: OutlineInputBorder( // Use OutlineInputBorder for border
+                        borderSide: BorderSide(color: Colors.white12), // Set the border color to grey
+                        borderRadius: BorderRadius.circular(10.0), // Adjust the border radius as needed
                       ),
                     ),
+                  ),
                 ),
 
                 Container(
@@ -86,26 +149,26 @@ class _DashBoardState extends State<DashBoard> {
         ),
 
         bottomNavigationBar: BottomNavigationBar(
-          type : BottomNavigationBarType.fixed,
+          type: BottomNavigationBarType.fixed,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: 'Home',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'History',
+              icon: Icon(Icons.settings),
+              label: 'settings',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'History',
+              icon: Icon(Icons.error),
+              label: 'Penalty',
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.person),
               label: 'Profile',
             ),
           ],
-          currentIndex: _selectedIndex,
+          currentIndex: _selectedPage,
           onTap: _onItemTapped,
         ),
       );
@@ -143,16 +206,16 @@ class _DashBoardState extends State<DashBoard> {
                         indent: 15.0,
                         endIndent: 15.0,
                       ),
-                      Column(
-                        children: <Widget>[
+                      ListTile(
+                        leading: Text(
+                            "Time", style: TextStyle(color: Colors.grey.withOpacity(0.5)),
+                        ),
+                        trailing: Text(
+                            "Date", style: TextStyle(color: Colors.grey.withOpacity(0.5)),
+                        ),
 
-                          ListTile(
-                            leading: Text("Date", style: TextStyle(color: Colors.grey.withOpacity(0.5)),),
-                            horizontalTitleGap: 10,
-                            trailing: Text("Time", style: TextStyle(color: Colors.grey.withOpacity(0.5)),),
-                          ),],
                       ),
-                      Row(
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           ElevatedButton(
